c49c1ec3ae14e8d620a9f44bf7b9d2e532f33373
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2019-11-10 22:17:57 +0100

    Add timeout when attempting to connect to database

--- src/WordFlip.Data/Repositories/WordFlipRepository.cs
+++ src/WordFlip.Data/Repositories/WordFlipRepository.cs
@@ -1,13 +1,14 @@
-﻿using System.Data.Common;
-
-namespace Wordsmith.WordFlip.Data.Repositories
+﻿namespace Wordsmith.WordFlip.Data.Repositories
 {
     using Entities;
 
     using Dapper;
 
+    using System;
     using System.Collections.Generic;
+    using System.Data.Common;
     using System.Data;
+    using System.Threading;
     using System.Threading.Tasks;
 
 
@@ -17,17 +18,22 @@ namespace Wordsmith.WordFlip.Data.Repositories
     public class WordFlipRepository : IWordFlipRepository<FlippedSentence>
     {
         private readonly DbConnection _connection;
-        private IDbConnection Connection
+        private async Task<IDbConnection> GetConnection()
         {
-            get
-            {
-                if (_connection.State != ConnectionState.Open)
-                {
-                    _connection.Open();
-                }
+            if (_connection.State != ConnectionState.Closed) return _connection;
 
-                return _connection;
+            using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(2));
+
+            try
+            {
+                await _connection.OpenAsync(cts.Token);
             }
+            catch (TaskCanceledException)
+            {
+                throw new TimeoutException($"Timed out when connecting to database {_connection.Database}. Make sure that the SQL Server instance is up and running.");
+            }
+
+            return _connection;
         }
 
 
@@ -48,24 +54,24 @@ namespace Wordsmith.WordFlip.Data.Repositories
         /// <param name="page">The page of results to return.</param>
         public async Task<IEnumerable<FlippedSentence>> GetLastSentences(int itemsPerPage, int page = 1)
         {
-            return await Connection.QueryAsync<FlippedSentence>(@"SELECT  *
+            return await (await GetConnection()).QueryAsync<FlippedSentence>(@"SELECT  *
 
-                                                                  FROM    ( SELECT ROW_NUMBER() OVER ( ORDER BY FS.Created DESC, FS.Id DESC ) AS RowNumber,
-                                                                                   FS.Id, FS.Sentence, FS.Created
+                                                                               FROM    ( SELECT ROW_NUMBER() OVER ( ORDER BY FS.Created DESC, FS.Id DESC ) AS RowNumber,
+                                                                                                FS.Id, FS.Sentence, FS.Created
 
-                                                                            FROM   FlippedSentences AS FS
-                                                                          ) AS RowConstrainedResult
-                                                                  WHERE RowNumber >= @min
-                                                                    AND RowNumber < @max
+                                                                                         FROM   FlippedSentences AS FS
+                                                                                       ) AS RowConstrainedResult
+                                                                               WHERE RowNumber >= @min
+                                                                                 AND RowNumber < @max
 
-                                                                  ORDER BY RowNumber",
+                                                                               ORDER BY RowNumber",
                 
 
-                                                                  new
-                                                                  {
-                                                                      min = (page - 1) * itemsPerPage   + 1,
-                                                                      max =  page      * itemsPerPage   + 1
-                                                                  });
+                                                                               new
+                                                                               {
+                                                                                   min = (page - 1) * itemsPerPage   + 1,
+                                                                                   max =  page      * itemsPerPage   + 1
+                                                                               });
         }
 
 
@@ -77,16 +83,16 @@ namespace Wordsmith.WordFlip.Data.Repositories
         /// <param name="sentence">The flipped sentence to persist.</param>
         public async Task<FlippedSentence> NewFlippedSentence(string sentence)
         {
-            return await Connection.QuerySingleAsync<FlippedSentence>(@"INSERT INTO FlippedSentences
+            return await (await GetConnection()).QuerySingleAsync<FlippedSentence>(@"INSERT INTO FlippedSentences
 
-                                                                       (Sentence)
+                                                                                    (Sentence)
 
-                                                                        OUTPUT INSERTED.*
+                                                                                     OUTPUT INSERTED.*
 
-                                                                        VALUES(@sentence)",
+                                                                                     VALUES(@sentence)",
                 
                 
-                                                                        new { sentence });
+                                                                                     new { sentence });
         }
     }
 }
