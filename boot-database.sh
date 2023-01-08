f555ba7845853869ee1926460f2c3fa61d774c28
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2023-01-08 16:50:24 +0100

    * Upgrade to SQL Server 2022 * Source db password from env variable * Bump test packages * Docker images

--- boot-database.sh
+++ boot-database.sh
@@ -1,2 +1,2 @@
-echo 'Starting up SQL Server 2019...'
+echo 'Starting up SQL Server 2022...'
 /opt/mssql/bin/sqlservr | ./setup-database.sh
--- setup-database.sh
+++ setup-database.sh
@@ -2,6 +2,6 @@
 sleep 25s
 
 echo 'Setting up database...'
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P January2018 -d master -i ./setup-database.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i ./setup-database.sql
 
 echo 'Database ready.'
