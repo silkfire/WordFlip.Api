7a84f37a9a6f9d64d2cfe1ed6c50ecae1bfd4ef0
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2024-07-28 01:29:14 +0200

    * Fix issues in SQL startup script * Bump test packages + Docker images

--- setup-database.sh
+++ setup-database.sh
@@ -1,7 +1,4 @@
-# Make sure the server has started before running the setup step
-sleep 25s
-
 echo 'Setting up database...'
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i ./setup-database.sql
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -d master -i ./setup-database.sql -C
 
 echo 'Database ready.'
